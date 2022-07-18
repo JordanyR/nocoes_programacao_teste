bd732e352c67b4ec9f9361bed80bc0de598926a5
unknown <jordanyRocha15@gmail.com>
2022-07-18 20:12:15 -0300

    ultima atualizacao do sistema

--- aula2.php
+++ aula2.php
@@ -1,34 +1,40 @@
 <?php
 print_r($_POST);
-// echo "Olá a todos !";
+
+// echo "Olá a todos!";
 if(isset($_POST['nome'])){
 
 }
 ?>
 <center>
-    <a> Oie </a><br>
-    <h1> Oie2</h1><br>
-    <h2> Oie3 </h2><br>
-    <a href="aula3.php"> Voltar </a> 
-<form action="" method="POST">
-    <label> 
-        <b style="color:red;">Informe o seu Nome: </b> 
-    <input type="text" name="nome"> <br><br>
+    <a>Oie</a><br>
+    <h1>Oie2</h1><br>
+    <h2>Oie3</h2><br>
+    <a href="aula3.php">Voltar</a>
+    <form action="" method="POST">
+        <label>
+            <b style="color:#9327B8;"> texto =</b>
+            <b style="color:red;">Informe o seu Nome:</b>
+        </label> 
+        <input type="text" name="nome"><br>
+        
+        <label>
+            <b>Informe o seu sobrenome:</b>
+        </label>
+        <input type="text" name="sobrenome"><br>
 
-    <label> 
-        <b> Informe o seu <b style="color:blue;"> Sobrenome: </b>
-    <input type="text" name="sobrenome"><br><br>
+        <label>
+            <b>Informe a sua idade:</b>
+        </label>
+        <input type="number" name="idade"><br>
 
-    <label> 
-        <b> <b style="color:orange;" > Informe a sua idade: </b>
-    <input type="number" name="idade"> <br><br>
+        <label>
+            <b>Informe a sua data de nascimento:</b>
+        </label>
+        <input type="date" name="nascimento"><br>
 
-    <label>
-      <b> Informe sua data de nascimento: </b>
-    <input type="date" name="data de nascimento"> <br><br>
-
-    <button type="submit"> Gravar </button> <br><br>
-</form>
-<img src="imagens/Natureza.png"width="160px" height="160px">
-<img src="imagens/praia.png"width="60%" height="60%">
+        <button type="submit">Gravar</button>
+    </form>
+    <!--  <img src="imagens/carro.png" width="100px" height="100px"> -->
+    <img src="imagens/carro.png" width="100%" height="300em">
 </center>
